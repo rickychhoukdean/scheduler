b7296c0dc389b66a3db632838495af70e6c7c045
rickychhoukdean <chhoukdean184@msn.com>
2019-12-03 22:13:34 -0500

    Change ws port

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -50,7 +50,7 @@ export default function useApplicationData() {
       .catch(err => err);
 
 
-    const webSocket = new WebSocket("ws://localhost:8001");
+    const webSocket = new WebSocket("ws:https://interviewscheduler.netlify.com/");
     webSocket.onopen = function() {
       webSocket.send(`Socket initialized`);
     };
